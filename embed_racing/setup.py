f1d28d8242ce7197ca1391598bc70a470d14cc10
p8reit <p8reit@yahoo.com>
2024-12-30 09:33:56 -0500

    adding setup

---
+++ embed_racing/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="enbed_racing",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],  # Add dependencies if required
+    description="A description of your package",
+    author="Your Name",
+    author_email="your.email@example.com",
+    url="https://github.com/p8reit/enbed_racing",
+)
